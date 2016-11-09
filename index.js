7257cf1460320b17d154a360964526c46ac2853e
sara golestaneh <sara.golestaneh@mail.utoronto.ca>
2016-11-09 12:56:28 -0500

    added the changes to index.js

--- index.js
+++ index.js
@@ -41,6 +41,104 @@ app.get('/users', function(req, res) {
 
 });
 
+app.get('/entry/:userID/:date', function(req, res) {
+
+    //This queries the database and returns the rows from the database
+	Integer id = req.queryParams("userID");
+	TimeStamp entryDate = req.queryParams("date");
+	client.query("SELECT * FROM skin.entry WHERE userID ="+ id +"and date ="+ entryDate +";", function (err, qres) {
+		if (err) {
+			console.log("error");
+		} else {
+			console.log("qres is " + qres);
+			console.log(qres.rows);
+			res.json(qres.rows);
+		}
+	});
+
+});
+
+//Post request for add entry
+/*app.post('/add-entry', function(req, res) {
+    var entryID = DEFAULT;//req.body.entryID;
+    var userID = 2;//req.body.userID;
+    var date = DEFAULT;// req.body.date;
+		var photoLocation = 'a';//req.body.photoLocation;
+		var entryDescription = 'hey';//req.body.entryDescription;
+		var rating = 4;//req.body.rating;
+
+		client.query('INSERT INTO "skin.entry" VALUES ($1, $2, $3, $4, $5, $6);',
+			 [entryID, userID, date, photoLocation, entryDescription,rating], function(err, result) {
+					 if (err) {
+							 console.log(err);
+					 } else {
+							 console.log("New entry inserted: " + entryID);
+
+					 }
+			 });
+});
+
+//Post request for edit entry
+app.post('/edit-entry', function(req, res) {
+    var entryID = DEFAULT;//req.body.entryID;
+    var userID = 3;//req.body.userID;
+    var date = DEFAULT;// req.body.date;
+		var photoLocation = 'b';//req.body.photoLocation;
+		var entryDescription = 'hey2';//req.body.entryDescription;
+		var rating = 5;//req.body.rating;
+
+		client.query('UPDATE "skin.entry" VALUES ($1, $2, $3, $4, $5, $6);',
+			 [entryID, userID, date, photoLocation, entryDescription,rating], function(err, result) {
+					 if (err) {
+							 console.log(err);
+					 } else {
+							 console.log("Updated Entry: " + entryID);
+
+					 }
+			 });
+});
+
+//var entryID = DEFAULT;//req.body.entryID;
+var userID = 2;//req.body.userID;
+//var date = DEFAULT;// req.body.date;
+var photoLocation = 'a';//req.body.photoLocation;
+var entryDescription = 'hey';//req.body.entryDescription;
+var rating = 4;//req.body.rating;
+
+client.query('INSERT INTO skin.entry VALUES (DEFAULT, $1, DEFAULT, $2, $3, $4);',
+	 [userID, photoLocation, entryDescription,rating], function(err, result) {
+			 if (err) {
+					 console.log(err);
+			 } else {
+					 console.log("New entry inserted");
+
+			 }
+	 });
+
+// listener part and one extra query:
+
+var query = 'SELECT * FROM skin.user;';
+client.query(query, function (err, qres) {
+	if (err) {
+		return console.log("error running query", err);
+	} else {
+		//console.log(qres.rows);
+		//console.log(JSON.stringify(qres.rows));
+	}
+});
+
+//Post request for edit entry
+
+		client.query('UPDATE skin.entry SET rating = 5;', function(err, result) {
+					 if (err) {
+							 console.log(err);
+					 } else {
+							 console.log("Updated Entry: ");
+
+					 }
+			 });
+*/
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
