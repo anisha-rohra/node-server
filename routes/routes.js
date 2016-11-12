fac78f9601a823beaeb6f26cf3e9d24734712c10
Shannon Lui <shannonlui@hotmail.com>
2016-11-12 12:47:13 -0500

    fixing routes async issue

--- routes/routes.js
+++ routes/routes.js
@@ -194,8 +194,7 @@ exports.postUser = function(req,res,next){
      var string = "SELECT * FROM skin.product" ;
      //var rows = queryGetDatabase(string,"Get Products");
      //res.json(rows);
-
-     client.query(string,function(err,qres){
+     client.query("SELECT * FROM skin.product", function(err,qres){
        if(err){
          console.log("Error in " + "Get Products");
        }
@@ -203,6 +202,11 @@ exports.postUser = function(req,res,next){
          res.json(qres.rows);
        }
      });
+     /*client.query("SELECT * FROM skin.product",function(err,qres){
+       console.log(qres.rows);
+       console.log(qres);
+       res.json(qres.rows);
+     });*/
    }
    else{
      return next();
