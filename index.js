4a28c665767357b8ba57be46713ed2efc8a9af42
Anisha Rohra <anisha.rohra@mail.utoronto.ca>
2016-11-29 12:58:32 -0500

    trying to identify what broke it

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use(bodyParser.urlencoded({ // Supports URL encoded bodies
 	extended:true
 }));
 
-/* The section below refers to the API all the routes that this API will support
+/*
+ * The section below refers to the API all the routes that this API will support
  * will be defined here. Developers should take a closer look to pre-requisites
  * and post-requisites for each one of the handlers of the HTTP endpoint
- *
  */
 
 app.get('/', function(request, response) {
@@ -69,132 +69,7 @@ app.post('/delete-my-product', routes.deleteMyProduct);
 
 app.post('/upload-photo', routes.uploadPhoto);
 
-app.get('/user-products', routes.getProductsByEntry);
-
-//app.get('/entry', routes.getEntryByIDAndDate);
-
-// GET Products:
-// Endpoint handles requests with queries:
-	// Queries: -	/products?prodid=id
-	//					-	/products?brand=brand
-	//					- /products?rating=rating
-	//					- /products?userid=userid
-// app.get('/products',routes.getProducts, routes.getProductById,
-// 										routes.getProductsByBrand, routes.getProductsByRating,
-// 										routes.getUserProducts);
-// app.post('/products',routes.addProduct);
-
-
-/*app.get('/entry/:userID/:date', function(req, res) {
-
-    //This queries the database and returns the rows from the database
-	var id = req.params.userID;
-	var date = req.params.date;
-	//TimeStamp entryDate = '2016-11-09 17:55:20.268058';
-	client.query("SELECT * FROM skin.entry where userid = "+ id + "and date = " + date + " ;", function (err, qres) {
-		if (err) {
-			console.log("error");
-		} else {
-		//	console.log("qres is " + qres);
-			console.log(qres.rows);
-		//	res.send(req.params.userID);
-		res.json(qres.rows);
-		}
-	});
-
-});*/
-
-//Post request for add entry
-/*app.post('/add-entry', function(req, res) {
-    var entryID = DEFAULT;//req.body.entryID;
-    var userID = 2;//req.body.userID;
-    var date = DEFAULT;// req.body.date;
-		var photoLocation = 'a';//req.body.photoLocation;
-		var entryDescription = 'hey';//req.body.entryDescription;
-		var rating = 4;//req.body.rating;
-
-		client.query('INSERT INTO "skin.entry" VALUES ($1, $2, $3, $4, $5, $6);',
-			 [entryID, userID, date, photoLocation, entryDescription,rating], function(err, result) {
-					 if (err) {
-							 console.log(err);
-					 } else {
-							 console.log("New entry inserted: " + entryID);
-
-					 }
-			 });
-});
-
-//Post request for edit entry
-app.post('/edit-entry', function(req, res) {
-    var entryID = DEFAULT;//req.body.entryID;
-    var userID = 3;//req.body.userID;
-    var date = DEFAULT;// req.body.date;
-		var photoLocation = 'b';//req.body.photoLocation;
-		var entryDescription = 'hey2';//req.body.entryDescription;
-		var rating = 5;//req.body.rating;
-
-		client.query('UPDATE "skin.entry" VALUES ($1, $2, $3, $4, $5, $6);',
-			 [entryID, userID, date, photoLocation, entryDescription,rating], function(err, result) {
-					 if (err) {
-							 console.log(err);
-					 } else {
-							 console.log("Updated Entry: " + entryID);
-
-					 }
-			 });
-});
-
-//var entryID = DEFAULT;//req.body.entryID;
-var userID = 2;//req.body.userID;
-//var date = DEFAULT;// req.body.date;
-var photoLocation = 'a';//req.body.photoLocation;
-var entryDescription = 'hey';//req.body.entryDescription;
-var rating = 4;//req.body.rating;
-
-client.query('INSERT INTO skin.entry VALUES (DEFAULT, $1, DEFAULT, $2, $3, $4);',
-	 [userID, photoLocation, entryDescription,rating], function(err, result) {
-			 if (err) {
-					 console.log(err);
-			 } else {
-					 console.log("New entry inserted");
-
-			 }
-	 });
-
-// listener part and one extra query:
-
-var query = 'SELECT * FROM skin.user;';
-client.query(query, function (err, qres) {
-	if (err) {
-		return console.log("error running query", err);
-	} else {
-		//console.log(qres.rows);
-		//console.log(JSON.stringify(qres.rows));
-	}
-});
-
-//Post request for edit entry
-
-		client.query('UPDATE skin.entry SET rating = 5;', function(err, result) {
-					 if (err) {
-							 console.log(err);
-					 } else {
-							 console.log("Updated Entry: ");
-
-					 }
-			 });
-*/
-
-/*client.query("INSERT INTO skin.myProduct VALUES (DEFAULT, 1, 1,  DEFAULT, DEFAULT, DEFAULT) ;",
-		function(err,qres){
-			if(err){
-				//Err is a map return the error to the user
-				console.log("Error\n");
-			}
-			else{
-				console.log("New product added to  my product\n");
-			}
-		});*/
+//app.get('/user-products', routes.getProductsByEntry);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
